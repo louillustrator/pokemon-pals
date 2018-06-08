cff96c431a7b13ae80e291b44a388d5e63be2b94
Dreval <somecheesewithyourwine@gmail.com>
2018-06-08 11:49:37 +0100

    About to edit poemon creation

--- game/game.js
+++ game/game.js
@@ -9,13 +9,37 @@ let makeTrainer = [
         name: 'trainer1',
         message: 'Trainer 1, please enter your name.',
         validate: function(value) {
-            let pass = value.match(/([A-Z, a-z])+/g);
-            //regex here isn't working right, needs checking
+            let pass = value.match(/^[A-Z, a-z]{1,8}$/g);
             if (pass) return true;
-            return ("That's not a name!")
-        },
+            return ("Invalid name!")
+        }
+    },
+    {
+        type: 'list',
+        name: 'pkmn1',
+        message: 'Trainer 1, please choose your Pokemon.',
+        choices: ['Bulbasaur', 'Charmander', 'Squirtle'], 
+    },
+    {
+        type: 'input',
+        name: 'trainer2',
+        message: 'Trainer 2, please enter your name.',
+        validate: function(value) {
+            let pass = value.match(/^[A-Z, a-z]{1,8}$/g);
+            if (pass) return true;
+            return ("Invalid name!")
+        }
+    },
+    {
+        type: 'list',
+        name: 'pkmn2',
+        message: 'Trainer 2, please choose your Pokemon.',
+        choices: ['Bulbasaur', 'Charmander', 'Squirtle'], 
     }
 ]
+
 inquirer.prompt(makeTrainer).then(answers => {
-    console.log('Game Over')
+    const train1 = new Trainer (answers.trainer1);
+    train1.catch(answers.pkmn1);
+    console.log(train1);
 })
